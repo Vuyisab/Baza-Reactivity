e9e29fd59778983fbd0982f0d8ee709d27deb7bf
Vuyisa Baza <vuyisa.baza@entelect.co.za>
2023-08-08 11:30:51 +0200

    added initial routing

--- vlient-app/src/index.tsx
+++ vlient-app/src/index.tsx
@@ -2,19 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "semantic-ui-css/semantic.min.css";
 import "./app/layout/styles.css";
-import App from "./app/layout/App";
 import reportWebVitals from "./reportWebVitals";
 import { StoreContext, store } from "./app/stores/store";
+import { RouterProvider } from "react-router";
+import { router } from "./app/router/Routes";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <StoreContext.Provider value={store}>
-      <App />
-    </StoreContext.Provider>
-  </React.StrictMode>
+  <StoreContext.Provider value={store}>
+    <RouterProvider router={router} />
+  </StoreContext.Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
