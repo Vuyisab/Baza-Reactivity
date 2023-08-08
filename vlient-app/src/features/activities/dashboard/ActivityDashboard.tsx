5dc634e7387af01b96c24140b62f2f74c1ecd1bd
Vuyisa Baza <vuyisa.baza@entelect.co.za>
2023-08-08 15:24:30 +0200

    Finished adding navigation

--- vlient-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ vlient-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Grid, GridColumn } from "semantic-ui-react";
 import ActivityList from "./ActivityList";
-import ActivityDetails from "../details/ActivityDetails";
-import ActivityForm from "../form/ActivityForm";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
+import LoaderComponent from "../../../app/layout/Loader";
 
 const ActivityDashboard = () => {
   const { activityStore } = useStore();
-  const { selectedActivity, editMode } = activityStore;
+  const { activityRegistry, loadActivities } = activityStore;
+
+  useEffect(() => {
+    if (activityRegistry.size <= 1) loadActivities();
+  }, [loadActivities, activityRegistry.size]);
+
+  if (activityStore.loadingInitial)
+    return <LoaderComponent content="Loading app" />;
 
   return (
     <Grid>
@@ -16,8 +22,7 @@ const ActivityDashboard = () => {
         <ActivityList />
       </GridColumn>
       <GridColumn width="6">
-        {selectedActivity && <ActivityDetails />}
-        {editMode && <ActivityForm />}
+        <h2>Activity filters</h2>
       </GridColumn>
     </Grid>
   );
