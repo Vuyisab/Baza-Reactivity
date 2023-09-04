fb9d2c2323b45bc37039875ff4221a1127b292b5
Vuyisa Baza <vuyisa.baza@entelect.co.za>
2023-09-04 20:08:42 +0200

    added attendance functionality

--- vlient-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ vlient-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Item, Button, Segment, Icon } from "semantic-ui-react";
+import { Item, Button, Segment, Icon, Label } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 import { format } from "date-fns";
+import ActivityListItemAttendee from "./ActivityListItemAttendee";
 
 interface Props {
   activity: Activity;
@@ -12,6 +13,14 @@ const ActivityListItem = ({ activity }: Props) => {
   return (
     <Segment.Group>
       <Segment>
+        {activity.Cancelled && (
+          <Label
+            attached="top"
+            color="red"
+            content="Cancelled"
+            style={{ textAlign: "center" }}
+          />
+        )}
         <Item.Group>
           <Item>
             <Item.Image size="tiny" circular src="/assets/user.png" />
@@ -19,7 +28,19 @@ const ActivityListItem = ({ activity }: Props) => {
               <Item.Header as={Link} to={`/activities/${activity.id}`}>
                 {activity.title}
               </Item.Header>
-              <Item.Description>Hosted by Bob</Item.Description>
+              <Item.Description>
+                Hosted by {activity.hostUsername}
+              </Item.Description>
+              {activity.isHost && (
+                <Label basic color="orange">
+                  You are hosting this activity
+                </Label>
+              )}
+              {activity.isGoing && !activity.isHost && (
+                <Label basic color="green">
+                  You are going to this activity
+                </Label>
+              )}
             </Item.Content>
           </Item>
         </Item.Group>
@@ -30,7 +51,9 @@ const ActivityListItem = ({ activity }: Props) => {
           <Icon name="marker" /> {activity.venue}
         </span>
       </Segment>
-      <Segment secondary>Attendees go here</Segment>
+      <Segment secondary>
+        <ActivityListItemAttendee attendees={activity.attendees!} />
+      </Segment>
       <Segment clearing>
         <span>{activity.description}</span>
         <Button
--- vlient-app/src/features/activities/form/ActivityForm.tsx
+++ vlient-app/src/features/activities/form/ActivityForm.tsx
@@ -3,7 +3,7 @@ import { Button, Header, Segment } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { Activity } from "../../../app/models/activity";
+import { ActivityFormValues } from "../../../app/models/activity";
 import LoaderComponent from "../../../app/layout/Loader";
 import { v4 as uuid } from "uuid";
 import { Formik, Form } from "formik";
@@ -16,25 +16,14 @@ import MyDateInput from "../../../app/common/form/MyDateInput";
 
 const ActivityForm = () => {
   const { activityStore } = useStore();
-  const {
-    createActivity,
-    updateActivity,
-    loading,
-    loadActivity,
-    loadingInitial,
-  } = activityStore;
+  const { createActivity, updateActivity, loadActivity, loadingInitial } =
+    activityStore;
 
   const { id } = useParams();
   const navigate = useNavigate();
-  const [activity, setActivity] = useState<Activity>({
-    id: "",
-    title: "",
-    description: "",
-    category: "",
-    city: "",
-    date: null,
-    venue: "",
-  });
+  const [activity, setActivity] = useState<ActivityFormValues>(
+    new ActivityFormValues()
+  );
 
   const validationSchema = Yup.object({
     title: Yup.string().required("The activity title is required"),
@@ -46,14 +35,18 @@ const ActivityForm = () => {
   });
 
   useEffect(() => {
-    if (id) loadActivity(id).then((activity) => setActivity(activity!));
+    if (id)
+      loadActivity(id).then((activity) =>
+        setActivity(new ActivityFormValues(activity))
+      );
   }, [id, loadActivity]);
 
-  const handleFormSubmit = (activity: Activity) => {
+  const handleFormSubmit = (activity: ActivityFormValues) => {
     if (!activity.id) {
-      activity.id = uuid();
-      createActivity(activity).then(() =>
-        navigate(`/activities/${activity.id}`)
+      let newActivity = { ...activity, id: uuid() };
+      console.log(newActivity);
+      createActivity(newActivity).then(() =>
+        navigate(`/activities/${newActivity.id}`)
       );
     } else {
       updateActivity(activity).then(() =>
@@ -96,12 +89,12 @@ const ActivityForm = () => {
               positive
               type="submit"
               content="Submit"
-              loading={loading}
+              loading={isSubmitting}
               disabled={isSubmitting || !dirty || !isValid}
             />
             <Button
               as={Link}
-              to="/activities"
+              to={"/activities"}
               floated="right"
               type="submit"
               content="Cancel"
