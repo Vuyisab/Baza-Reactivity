10e0e6b8cd6fef9446f8ecaabb0438674a9d37eb
Vuyisa Baza <vuyisa.baza@entelect.co.za>
2023-09-08 07:39:41 +0200

    added image api

---
+++ Application/Photos/Delete.cs
@@ -0,0 +1,45 @@
+using Application.Core;
+using Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Persistence;
+
+namespace Application.Photos
+{
+    public class Delete
+    {
+        public class Command : IRequest<Result<Unit>>
+        {
+            public string Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<Unit>>
+        {
+            private readonly IPhotoAccessor _photoAccessor;
+            private readonly DataContext _context;
+            private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext context, IPhotoAccessor photoAccessor, IUserAccessor userAccessor)
+            {
+                _userAccessor = userAccessor;
+                _context = context;
+                _photoAccessor = photoAccessor;
+
+            }
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var user = await _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(x => x.UserName == _userAccessor.GetUserName());
+                if (user == null) return null;
+
+                var photo = user.Photos.FirstOrDefault(x => x.Id == request.Id);
+                var result = await _photoAccessor.DeletePhoto(photo.Id);
+
+                if (result == null) return Result<Unit>.Failure("Problem deleting photo from Cloudinary");
+                user.Photos.Remove(photo);
+                var success = await _context.SaveChangesAsync() > 0;
+                if (success) return Result<Unit>.Success(Unit.Value);
+                return Result<Unit>.Failure("Problem deleting photo from Api");
+            }
+        }
+    }
+}
